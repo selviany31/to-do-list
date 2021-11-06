bbd61e1c8cb82f426384100835d2c06daa7c5b3c
selviany31 <shelvy.any@gmail.com>
2021-11-06 12:36:28 +0800

    feat: adding card commponent

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Card from "./Components/Card";
 import Header from "./Components/Header";
 import './Styles/style.css'
 
@@ -5,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div style={{display: "flex"}}>
+      <Card styleName="card-border card-bg-orange"/>
+      <Card styleName="card-border card-bg-blue"/>
+      <Card styleName="card-border"/>
+      <Card styleName="card-border"/>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/Card.css'
+
+const Card = ({ styleName }) => {
+    return (
+        <div className={styleName}>
+            lalalal
+        </div>
+    )
+}
+
+export default Card
